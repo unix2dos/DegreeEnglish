983b6ddddd2af364e14005330ccf060b75c27f4c
liuwei <levonfly@gmail.com>
2019-08-28 00:13:08 +0800

    add topic

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from pkg.func import hello
+from pkg.topic import Topic
+import itchat
+
+
+def send_group(group, msg):
+    rooms = itchat.get_chatrooms(update=True)
+    rooms = itchat.search_chatrooms(name=group)
+    if not rooms:
+        print("None group found")
+    else:
+        itchat.send(msg, toUserName=rooms[0]["UserName"])
 
 
 if __name__ == '__main__':
-    hello()
+    a, b, c, d = Topic().start()
+    msg = "今日学位英语: (难度" + d + ")\n\n" + a + "\n\n" + b
+    # print(msg)
+    # print(a,"\n",b,"\n",c,"\n",d)
+    itchat.auto_login(hotReload=False)
+    send_group(u"爱吃早餐群", msg)
+    itchat.run()
+
+
 
--- pkg/topic.py
+++ pkg/topic.py
@@ -1,56 +1,53 @@
+import requests
+
+
 class Topic(object):
     def __init__(self):
-        pass
+        self.imei = "6f2252fcb3ed40bd90a618735970a2a9"
 
-    def Start(self):
-        pass
+    def start(self):
+        knowId = self.get_knowId()
+        res = self.get_topic(knowId)
+        self.do_topic(res)
+        return self.send(res)
 
 
+    def get_knowId(self):
+        url = "http://adenglish.sikai.net.cn/xwenglish/na/exercise/getStudyPath"
+        params = {"imei":self.imei, "itemType": 1}
+
+        resp = requests.post(url, params=params).json()
+        if resp["error"] != None :
+            return
+
+        return resp["result"]["tkId"]
 
 
-#
-#
-#
-# http://adenglish.sikai.net.cn/xwenglish/na/exercise/getStudyPath
-#
-# imei=27189abff26d43668b07daebcb97c854&itemType=1
-#
-#
-#
-# {
-#
-# 	"timestamp": "2019-08-27T14:13:58.701+0000",
-#
-# 	"status": 200,
-#
-# 	"message": "服务调用成功",
-#
-# 	"error": null,
-#
-# 	"result": {
-#
-# 		"knowId": 126,
-#
-# 		"tkId": 2587,
-#
-# 		"knowName": "动名词",
-#
-# 		"masterProgress": 0,
-#
-# 		"remainingPraxis": 10
-#
-# 	}
-#
-# }
-#
-#
-#
-# http://adenglish.sikai.net.cn/xwenglish/na/exercise/getAdaptivePraxis
-#
-#
-#
-#
-#
-# imei=6f2252fcb3ed40bd90a618735970a2a9&knowId=2587
-#
-#
+    def get_topic(self, knowId):
+        url = "http://adenglish.sikai.net.cn/xwenglish/na/exercise/getAdaptivePraxis"
+        params = {"imei":self.imei, "knowId": knowId}
+
+        resp = requests.post(url, params=params).json()
+        if resp["error"] != None:
+            return
+
+        return resp["result"]
+
+    def do_topic(self, res):
+        url = "http://adenglish.sikai.net.cn/xwenglish/na/exercise/submitTest4OnePraxis"
+        item_id = res["content"][0]["itemId"]
+        answer_index = res["content"][0]["answerIndex"]
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        data = "answers=%5B%0A%20%20%7B%0A%20%20%20%20%22doPraxisTime%22%20%3A%2013101%2C%0A%20%20%20%20%22itemType%22%20%3A%201%2C%0A%20%20%20%20%22standerAns%22%20%3A%20%224%22%2C%0A%20%20%20%20%22itemId%22%20%3A%20%22{}%22%2C%0A%20%20%20%20%22userAnsIndex%22%20%3A%20%22{}%22%0A%20%20%7D%0A%5D&exerciseType=1&imei=6f2252fcb3ed40bd90a618735970a2a9&paperId=&status=1".format(item_id,answer_index)
+        requests.post(url, data=data, headers=headers)
+
+
+    def send(self, res):
+        content = res["content"][0]
+        topic = content["topic"]
+        options = ""
+        for k, v in enumerate(content["options"]):
+            options += chr(k+65) + " "+  v["content"] + "\n\n"
+        analysis = content["analysis"]["analysisContent"]
+        difficulty = content["difficulty"]
+        return topic, options, analysis, difficulty
