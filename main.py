b25243c408f9e7815289511e862c1727a2c54b36
liuwei <levonfly@gmail.com>
2019-08-27 23:12:27 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from pkg.func import hello
+
+
+if __name__ == '__main__':
+    hello()
+
---
+++ pkg/topic.py
@@ -0,0 +1,56 @@
+class Topic(object):
+    def __init__(self):
+        pass
+
+    def Start(self):
+        pass
+
+
+
+
+#
+#
+#
+# http://adenglish.sikai.net.cn/xwenglish/na/exercise/getStudyPath
+#
+# imei=27189abff26d43668b07daebcb97c854&itemType=1
+#
+#
+#
+# {
+#
+# 	"timestamp": "2019-08-27T14:13:58.701+0000",
+#
+# 	"status": 200,
+#
+# 	"message": "服务调用成功",
+#
+# 	"error": null,
+#
+# 	"result": {
+#
+# 		"knowId": 126,
+#
+# 		"tkId": 2587,
+#
+# 		"knowName": "动名词",
+#
+# 		"masterProgress": 0,
+#
+# 		"remainingPraxis": 10
+#
+# 	}
+#
+# }
+#
+#
+#
+# http://adenglish.sikai.net.cn/xwenglish/na/exercise/getAdaptivePraxis
+#
+#
+#
+#
+#
+# imei=6f2252fcb3ed40bd90a618735970a2a9&knowId=2587
+#
+#
